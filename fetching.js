9d2ba4371ab283cd075c62a222729bcf63455a48
iamvetle <vetle122@gmail.com>
2025-11-07 17:33:12 +0100

    Made a seperate guessing bank, which is bigger than possible answers.

--- fetching.js
+++ fetching.js
@@ -1,7 +1,7 @@
 var wordleWord;
 let fiveWordsdict;
 
-fetch("/five-word-dict.json")
+fetch("/answer-bank.json")
 	.then((response) => {
 		if (!response.ok) {
 			throw new Error(
@@ -13,6 +13,21 @@ fetch("/five-word-dict.json")
 	.then((data) => {
 		const randomIndex = Math.floor(Math.random() * data.length);
 		wordleWord = data[randomIndex].toUpperCase();
+	})
+	.catch((error) => {
+		console.error("There was a problem with the fetch operation:", error);
+	});
+
+fetch("/quess-bank.json")
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(
+				"Network response was not ok: " + response.statusText
+			);
+		}
+		return response.json(); // Already parsed JSON
+	})
+	.then((data) => {
 		fiveWordsdict = data;
 	})
 	.catch((error) => {
